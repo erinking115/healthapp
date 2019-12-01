9b0de75ac63b4b5d3aa1743085afef5461eff6de
erinking115 <56370530+erinking115@users.noreply.github.com>
2019-12-01 18:12:13 -0500

    Add files via upload

---
+++ Health App/js/script.js
@@ -0,0 +1,8 @@
+(function(){
+  // Footer copyright statement
+    var d = new Date();
+    var y = d.getFullYear();
+    var t = document.createTextNode(y);
+    var c = document.getElementById('copy')
+    c.appendChild(t);
+})();
